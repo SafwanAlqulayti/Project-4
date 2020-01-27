e4dfbd8ebcd656e31cfccdc9a292997d769a802a
SarahA45 <59462868+SarahA45@users.noreply.github.com>
2020-01-27 15:14:24 +0300

    first db test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app=express()
 const mongoose=require('mongoose')
 const bodyParser=require('body-parser')
 const path=require('path')
-const config = require('config');
+//const config = require('config');
 
 
 
@@ -23,23 +23,35 @@ app.use('/api/items',items)
 //BodyParser Middleware
 
 
-//const db = require('./config/db');
+///const db = require('./config/db');
+const db='mongodb+srv://SarahMA:035212212Ss.$@cluster0-v7ewu.mongodb.net/test?retryWrites=true&w=majority'
 
-//const mongoURI=process.env.MONGODB_URI||'mongodb://localhost/shopping-list'
+// const mongoURI=process.env.MONGODB_URI||'mongodb://SarahMA:035212212sS.$@cluster0-v7ewu.mongodb.net/shopping'
 
-// mongoose.connect(db,{useNewUrlParser: true,  useCreateIndex: true}).then(()=>console.log('MongoDB connected'))
-// .catch(error=>console.log(error))
+ mongoose.connect(db,{useNewUrlParser: true,  useCreateIndex: true}).then(()=>console.log('MongoDB connected'))
+ .catch(error=>console.log(error))
 
-const db = config.get('mongoURI');
+// //const db = config.get('mongoURI');
 
-// Connect to Mongo
-mongoose
-  .connect(db, { 
-    useNewUrlParser: true,
-    useCreateIndex: true
-  }) // Adding new mongo url parser
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+// // Connect to Mongo
+// mongoose
+//   .connect(mongoURI, { 
+//     useNewUrlParser: true,
+//     useCreateIndex: true
+//   }) // Adding new mongo url parser
+//   .then(() => console.log('MongoDB Connected...'))
+//   .catch(err => console.log(err));
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb://localhost/shopping-shopping";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 if (process.env.NODE_ENV === 'production') {
     // Set static folder

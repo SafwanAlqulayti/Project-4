26683999ae091957c3f6f258deb81e3886f01988
Sarah <alahmadisr@gmail.com>
2020-02-06 06:32:49 +0300

    deploy

--- client/src/store.js
+++ client/src/store.js
@@ -11,7 +11,8 @@ const middleware=[thunk]//middle ware we're gonna use
 
 const store=createStore(rootReducer,initialState,compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 ))
 store.dispatch(loadUser())
 //store.dispatch(getUserRequests())

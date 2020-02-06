5ef43c8c1dc126279ee02136d5856c0b86fb692b
Sarah <alahmadisr@gmail.com>
2020-02-06 06:55:41 +0300

    deploy

--- client/src/store.js
+++ client/src/store.js
@@ -2,29 +2,18 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 import {loadUser} from './actions/authActions'
+import { getUserRequests } from './actions/requestActions'
 
 
 const initialState={}
 
 const middleware=[thunk]//middle ware we're gonna use
 
-if(process.env.NODE_ENV === 'production') {
-    store = createStore(rootReducer, initialState, compose(
-        applyMiddleware(...middleware)
-    ));
-} else {
-    store = createStore(rootReducer, initialState, compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ));
-}
-
-// const store=createStore(rootReducer,initialState,compose(
-//     applyMiddleware(...middleware),
-//     //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-// ))
-
+const store=createStore(rootReducer,initialState,compose(
+    applyMiddleware(...middleware),
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+))
 
 store.dispatch(loadUser())
 

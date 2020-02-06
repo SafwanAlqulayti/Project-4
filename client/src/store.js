7115a0f2537ce8a1f50d9613ae094c1338aaf47e
Sarah <alahmadisr@gmail.com>
2020-02-06 06:42:09 +0300

    deploy

--- client/src/store.js
+++ client/src/store.js
@@ -1,20 +1,36 @@
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers'
-import {loadUser} from './actions/authActions'
-import { getUserRequests } from './actions/requestActions'
+// import {createStore, applyMiddleware, compose} from 'redux'
+// import thunk from 'redux-thunk'
+// import rootReducer from './reducers'
+// import {loadUser} from './actions/authActions'
+// import { getUserRequests } from './actions/requestActions'
 
 
-const initialState={}
+// const initialState={}
 
-const middleware=[thunk]//middle ware we're gonna use
+// const middleware=[thunk]//middle ware we're gonna use
 
-const store=createStore(rootReducer,initialState,compose(
-    applyMiddleware(...middleware),
-    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-))
+// const store=createStore(rootReducer,initialState,compose(
+//     applyMiddleware(...middleware),
+//     //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+// ))
+
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default () => {
+  const store = createStore(
+    combineReducers({
+      // your reducers here...
+    }),
+    composeEnhancers(applyMiddleware(thunk))
+  );
+
+  return store;
+};
 store.dispatch(loadUser())
-//store.dispatch(getUserRequests())
 
-export default store
+//export default store
